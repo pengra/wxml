b84c6d19b60a18e7c6a5d2fcb79a2e0b08574dae
Zachary Barnes <33704301+zbarnes@users.noreply.github.com>
2019-01-19 18:29:19 -0800

    Add files via upload
    Basic layout of model and algorithm for Iowa

---
+++ IowaModel.py
@@ -0,0 +1,57 @@
+
+#Need to figure out how to pass in arguments 
+from rakan import PyRakan as BaseRakan
+import random
+
+class MarcovChainMonteCarlo(BaseRakan):
+    """
+    A MCMC method for the state of Iowa
+    We hope to generalize code for any state
+
+    """
+    #declare variables
+    float CompactEnergy 
+    float PopEnergy
+    int alpha
+    int beta
+
+#initialize starting point at random precinct 
+    startPoint = random.randint(0, precinct)
+
+#Calculate current energies
+    currentCompac = self.getcompactness(startPoint)
+    currentPopEng = self.getpopenergy(startPoint)
+
+#propose move 
+	precinct, district = self.propose_random_move(startPoint)
+
+#calculate candidate energies
+	candCompac = self.getcompactness(precinct)
+	candEnergy = self.getpopenergy(precinct)
+
+#calculate acceptance ratio 
+	u = uniform(0,1)
+
+#acceptance ratio
+	f = exp(alpha*currentCompac + beta*currentPopEng)
+	fprime = exp(alpha*candCompac + beta*candEnergy)
+	a = fprime/f
+
+#accept or reject
+if u <= a:
+	#accept
+	self.make_move(precinct, district)
+else:
+	#reject
+	break
+
+     
+
+
+
+
+
+
+
+
+
