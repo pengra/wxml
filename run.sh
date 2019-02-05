37849c78131b939cb723966eb744ee53e33c5db2
qwergram <npengra317@gmail.com>
2019-02-05 14:44:09 -0800

    update run script

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
+rm -rf build/
 rm -rf rakan/build/
 rm -rf rakan/cython_debug/
 rm -r rakan/*.*o
 rm -rf rakan/__pycache__/
 rm -r rakan/*.html
 rm -r rakan/rakan.cpp
-cd rakan
-touch rakan.pyx
-touch rakan.pxd
-python setup.py build_ext --inplace
-cd ..
+touch rakan/rakan.pyx
+touch rakan/rakan.pxd
+python setup.py build_ext
 python rakan/
