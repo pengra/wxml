c04a886d746a6cef56507f9bae47e573fbf1ffbd
qwergram <npengra317@gmail.com>
2019-02-28 18:53:06 -0800

    linting

--- rakan/independence_tester.py
+++ rakan/independence_tester.py
@@ -6,10 +6,10 @@ from base import BaseRakan as TestRakan
 from random_sequence_tests import r_value_independence_test
 from decimal import Decimal
 
-TOLERANCE = 0.1 # tolerance to determine whether the sequence was independent
-RID1 = 82 # Chosen b/c they are in the middle of the IOWA map
-RID2 = 48 # Chosen b/c they are in the middle of the IOWA map
-PRINT = True # Boolean to print the stepsize and correlation value
+TOLERANCE = 0.1  # tolerance to determine whether the sequence was independent
+RID1 = 82  # Chosen b/c they are in the middle of the IOWA map
+RID2 = 48  # Chosen b/c they are in the middle of the IOWA map
+PRINT = True  # Boolean to print the stepsize and correlation value
 
 '''
 Description: This method creates the sequence from the walk specified by 'path_name'
@@ -20,11 +20,13 @@ Input:
 Output:
     - sequence of 0's and 1's
 '''
+
+
 def get_sequence_from_file(path_name, rid1, rid2):
     final_result_report = []
 
     # Create Rakan
-    rakan = TestRakan(0,0)
+    rakan = TestRakan(0, 0)
     rakan.read_nx(path_name+"/save.dnx")
     moves = json.load(open(path_name+"/moves.json"))
 
@@ -36,8 +38,10 @@ def get_sequence_from_file(path_name, rid1, rid2):
             continue
         precinct_id, old_district_id, new_district_id = moves[i][-1]
         rakan.move_precinct(precinct_id, old_district_id)
-        final_result_report = [rakan.precinct_in_same_district(rid1, rid2)]+final_result_report;
+        final_result_report = [rakan.precinct_in_same_district(
+            rid1, rid2)]+final_result_report
     return final_result_report
+
 
 '''
 Description: This method tests the independence of the walk given report path_name
@@ -49,14 +53,18 @@ Input:
 Output:
     - the correlation value of the sequence
 '''
+
+
 def indpendence_test_from_report_file(path_name, rid1, rid2, step_size):
     sequence = get_sequence_from_file(path_name, rid1, rid2)
     try:
         v = r_value_independence_test(sequence, step_size)
         return v
     except Exception as e:
-        if PRINT: print(e.message())
-        return -10 # Invalid input exception
+        if PRINT:
+            print(e.message())
+        return -10  # Invalid input exception
+
 
 '''
 Description: This method finds the ideal step_size for the given sequence
@@ -70,6 +78,8 @@ Note:
     - when the PRINT bool is True, it creates a graph
     - it tries all the possible stepsizes. Could be optimized further
 '''
+
+
 def find_opt_stepsize(path_name, rid1, rid2):
     step_to_corr = []
     warnings.filterwarnings("ignore", category=RuntimeWarning)
@@ -77,12 +87,12 @@ def find_opt_stepsize(path_name, rid1, rid2):
     min_step = 1
     min_r_val = 1
     print(sequence)
-    for i in range(1,len(sequence) - 1):
+    for i in range(1, len(sequence) - 1):
         if numpy.cov(sequence) == 0:
-            step_to_corr.append(numpy.nan);
-            continue;
+            step_to_corr.append(numpy.nan)
+            continue
         v = abs(r_value_independence_test(sequence, i))
-        step_to_corr.append(v);
+        step_to_corr.append(v)
 
         if v < min_r_val:
             min_step = i
@@ -95,6 +105,7 @@ def find_opt_stepsize(path_name, rid1, rid2):
         plt.savefig(path_name+'/step_size_to_corr.png')
     return min_step
 
+
 '''
 Description: This method determines whether the walk was independent or not
 Input:
@@ -102,9 +113,14 @@ Input:
 Output:
     - True or False
 '''
+
+
 def test(report_folder_name, rid1=RID1, rid2=RID2):
     min_step = find_opt_stepsize(report_folder_name, rid1, rid2)
-    cor = indpendence_test_from_report_file(report_folder_name, rid1, rid2, min_step)
-    if cor == -10: return False
-    if PRINT: print("Step Size: {}  Correlation: {}".format(min_step, cor))
+    cor = indpendence_test_from_report_file(
+        report_folder_name, rid1, rid2, min_step)
+    if cor == -10:
+        return False
+    if PRINT:
+        print("Step Size: {}  Correlation: {}".format(min_step, cor))
     return cor < TOLERANCE
