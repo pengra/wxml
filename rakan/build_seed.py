69dac5d4c405a2e9f180eeb80160f8c285c35b23
Langley DeWitt <langleydewitt@gmail.com>
2019-04-11 14:41:05 -0700

    fixed bugs in spuers.py

--- rakan/build_seed.py
+++ rakan/build_seed.py
@@ -1,7 +1,7 @@
 import supers
 
-supers.build_supers('WA.dnx', 300, 10, 'super_map.csv', 'supers.dnx')
-supers.translate('supers.dnx', 'super_map.csv', 'WA.dnx', 'wa_seed.dnx')
+supers.build_supers('WA2.dnx', 300, 10, 'super_map.csv', 'supers.dnx')
+supers.translate('supers.dnx', 'super_map.csv', 'WA2.dnx', 'wa_seed.dnx')
 
 r = supers.Rakan()
 r.read_nx('wa_seed.dnx')
